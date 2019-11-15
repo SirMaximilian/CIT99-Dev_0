b1895c899a80ae08283da3b5481f49d919aa2437
dlinano <gbarr6622@gmail.com>
2019-11-15 15:03:39 -0800

    work files push

---
+++ dataHelper.py
@@ -0,0 +1,32 @@
+
+
+
+def write_frame(path, frame, indexer, x_cord_label, y_cord_label):
+    filename = F'img.{next(indexer)}.{x_cord_label}.{y_cord_label}.jpg'
+    status = cv2.imwrite(path + '/' + filename, frame)
+    return
+
+def imgcrop224(frame):
+    # crops given image frame to be 224 by 224
+    y = frame.shape[0]
+    x = frame.shape[1]
+    cy = y - 224
+    cx = x - 224
+    print 
+    return frame[int(cy/2):int(cy/2)+224, int(cx/2):int(cx/2)+224]
+
+def label_parser(filename, label_num = 3, delimiter = '.'):
+    # parses filename and returns list of labels seprated by delimiter
+    temp = 0
+    indexs = []
+    for i in range(label_num):
+        indexs.append(filename.find(delimiter,temp) + 1)
+        temp = filename.find(delimiter,temp) + 1
+    indexs.append(len(filename)+1)
+    return [int(filename[indexs.pop(0):indexs[0]-1]) for i in range(label_num) ]
+
+def counter(start_index=0):
+    n=0+start_index
+    while True:
+        yield n
+        n+=1
